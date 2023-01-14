873a97319be1d5c2a87621d2a1ca6f4969ec8868
chilivlz <johanachilito2@gmail.com>
2023-01-13 20:18:52 -0500

    primer commit

---
+++ js/products.js
@@ -0,0 +1,38 @@
+
+// Imagenes de ilustraciones del ShopArt//
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Medita",
+    precio: 50,
+    img:
+    "https://i.pinimg.com/564x/e0/7b/f2/e07bf22c1b0c4088e0763604c40ddd0c.jpg",
+    cantidad: 1,
+    
+  },
+  {
+    id: 2,
+    nombre: "Luna",
+    precio: 100,
+    img:
+      "https://i.pinimg.com/564x/b9/7f/46/b97f46b43864d4ca104561255931a1b0.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Mariposa Estelar",
+    precio: 150,
+    img:
+      "https://i.pinimg.com/originals/e5/78/d6/e578d6f5c9573e95248f86f3939f2eab.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Constelacion",
+    precio: 200,
+    img:
+      "https://i.pinimg.com/564x/35/d7/04/35d704574f3a8d224c66dc5ad1936c10.jpg",
+    cantidad: 1,
+  },
+];
